49ccee01525818805d896d1adc4a25163f344e1b
vsrinfinite <vsrinfinite9@gmail.com>
2026-04-30 12:28:22 +0530

    export public API interface for the engine

---
+++ src/backend/index.ts
@@ -0,0 +1,21 @@
+export * from './types';
+export * from './symbolMap';
+export * from './ast';
+export { tokenize, LexerError } from './lexer';
+export { parse, ParseError } from './parser';
+export { normalize, areEquivalent } from './normalizer';
+export { validateModel, getSuccessors, getAtomsAtWorld } from './model';
+export {
+  isReflexive, isSymmetric, isTransitive, isSerial, isEuclidean,
+  isFunctional, isPartialFunctional, isEmpty, isDiscrete, isDense,
+  isConvergent, isWellFounded,
+  enforceReflexive, enforceSymmetric, enforceTransitive, enforceSerial, enforceEuclidean,
+  checkConstraint, enforceConstraint, checkAllConstraints, enforceAllConstraints,
+} from './constraints';
+export {
+  PRESET_DEFINITIONS, detectConflicts, resolveProfile, matchPreset,
+  CONSTRAINT_CATEGORIES,
+} from './logicProfile';
+export { evaluate } from './checker';
+export { modelToDot } from './dot';
+export { checkFormula } from './service';
